be719014360f5b49344aa80c164702d508867ca6
Blair Wright <blairbwright@gmail.com>
2015-02-18 15:04:54 -0800

    new

--- model/config.php
+++ model/config.php
@@ -1,7 +1,14 @@
 <?php
+require_once(__DIR__ . "/database.php");
+session_start();
+
 $path = "/BlairBlog/";
 
 $host = "localhost";
    $username = "root";
    $password  = "root";
    $database = "blog_db";
+   
+   if(!isset($_SESSION["connection"])) {
+   $connection = new Database($host, $username, $password, $database);
+   $_SESSION["connection"] = $connection;}

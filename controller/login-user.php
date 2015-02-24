03abae1e28f78260dd041b11aa403dc41c8df9ef
Blair Wright <blairbwright@gmail.com>
2015-02-24 15:04:39 -0800

    new

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,4 +1,8 @@
 <?php
-echo "Successfully logged in user";
+require_once(__DIR__ . "/../model/config.php");
+
+$username  = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+
+$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
 
 
